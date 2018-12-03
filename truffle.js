7d0c3e94e75bea36c5af13bf1b46e26d59a8579e
roleengineer <roleengineer@gmail.com>
2018-12-03 14:51:21 +0200

    commit local project

---
+++ truffle.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    ropsten: {
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        process.env.ROPSTEN_URL),
+      network_id: '3',
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
